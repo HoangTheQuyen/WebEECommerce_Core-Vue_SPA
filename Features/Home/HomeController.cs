05e5ca1a77190f97e3568737b18a98b58bc60c39
HoangTheQuyen <38252060+HoangTheQuyen@users.noreply.github.com>
2018-11-03 22:46:39 +0700

    update 1
    update 1

--- Features/Home/HomeController.cs
+++ Features/Home/HomeController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebEECommerce_Core_Vue_SPA.Controllers
+namespace ECommerce.Controllers
 {
     public class HomeController : Controller
     {
--- Startup.cs
+++ Startup.cs
@@ -2,13 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ECommerce.Data;
+using ECommerce.Data.Entities;
+using ECommerce.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace WebEECommerce_Core_Vue_SPA
+namespace ECommerce
 {
     public class Startup
     {
@@ -22,7 +28,22 @@ namespace WebEECommerce_Core_Vue_SPA
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<EcommerceContext>(options =>                        
+                options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<AppUser, AppRole>()
+                .AddEntityFrameworkStores<EcommerceContext>()
+                .AddDefaultTokenProviders();
+
+
             services.AddMvc();
+
+            services.Configure<RazorViewEngineOptions>(options =>
+            {
+                options.ViewLocationExpanders.Add(new FeatureLocationExpander());
+            });
+
+            DbContextExtensions.UserManager = services.BuildServiceProvider().GetService<UserManager<AppUser>>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
